1127173eaa4f15e18391f4e9137625d6f5f1480a
Nishan Tamang <n.tamang@paicon.com>
2025-08-24 18:18:13 +0200

    add request handler in gem

--- bat_chest/lib/bat_chest.rb
+++ bat_chest/lib/bat_chest.rb
@@ -1,8 +1,45 @@
 # frozen_string_literal: true
 
+require "socket"
 require_relative "bat_chest/version"
 
 module BatChest
   class Error < StandardError; end
-  # Your code goes here...
+  class ParseError < Error; end
+end
+
+class BatChest::Request
+  attr_reader :url
+
+  def initialize(socket)
+    parse_req socket.gets
+
+    head = String.new
+
+    loop do
+      line = socket.gets
+      head << line.chomp("\r").chomp("\n") << "\n"
+      break if line.strip == ""
+    end
+
+    parse_headers(head)
+  end
+
+  def parse_req(line)
+    @method, @url, rest = line.split(/\s/, 3)
+
+    raise BatChest::ParseError.new("Can't parse request") unless rest =~ %r{HTTP/(\d+)\.(\d+)}
+
+    @http_version = "#{::Regexp.last_match(1)}.#{::Regexp.last_match(2)}"
+  end
+
+  def parse_headers(text)
+    @headers = {}
+    text.lines.each do |line|
+      break if line.strip.empty?
+
+      field, value = line.split(":", 2)
+      @headers[field.downcase] = value.strip
+    end
+  end
 end
